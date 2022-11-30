1501091e35ec47e5f65518b2641803a64c5b18ab
Felipe Lustosa <felipelustosaa6@gmail.com>
2022-11-30 09:17:29 -0300

    home inputs style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,9 +9,15 @@ export function Home(props: HomeProps) {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto" />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
           <label htmlFor="minutesAmount">Durante</label>
-          <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+          <MinutesAmountInput type="number" step={5} min={5} max={60} id="minutesAmount" placeholder="00" />
 
           <span>minutos.</span>
         </FormContainer>
